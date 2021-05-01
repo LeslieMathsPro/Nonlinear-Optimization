7afbc3cc70691d100c28298972d65509d8fb8dbc
liweiXU <liwei.xu@etu.emse.fr>
2021-05-01 19:05:04 +0200

    Update recursive_max.m

--- recursive_max.m
+++ recursive_max.m
@@ -9,13 +9,19 @@ function f = recursive_max(v)
     end
 end
 
-function f = recursive_max(v)
-    if length(v) <= 1
-        f = v;
+function mx = recursive_max(v)
+    if length(v) == 1 
+        mx = v(1);
     else
-        f = v(1);
-        if f < recursive_max(v(2:end));
-            f = recursive_max(v(2:end));
-        end
+        % Each recursion, v(2:end) becomes smaller by 1-element
+        mx = bigger(v(1),recursive_max(v(2:end)));
+    end
+end
+% Cannot use the max function. Use helper function to return the larger of 
+% two element
+function c = bigger(a,b)
+    c = a;
+    if a < b
+        c = b;
     end
 end
